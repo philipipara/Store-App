c822ab9c5e68a4573674de5683dec3987bbd956b
philipipara <54330369+philipipara@users.noreply.github.com>
2020-06-29 10:11:37 -0700

    admin pages

---
+++ client/src/admin/manageCategories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Base from "../core/Base";
+
+
+const ManageCategories = ()  => {
+    return (
+        <Base>
+
+        </Base>
+    )
+}
+
+export default ManageCategories;
