0345bd44fec4c32ad457b1117462dd9ea271877b
philipipara <54330369+philipipara@users.noreply.github.com>
2020-07-27 05:58:36 -0700

    fixed error when signed in not reading user.name

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -12,7 +12,7 @@ const currentTab = (history, path) => {
   }
 };
 
-const {user} = isAuthenticated();
+
 // const [reload, setReload] = useState(false)
 
 // useEffect(() => {
@@ -20,99 +20,104 @@ const {user} = isAuthenticated();
 // }, [reload]);
 
 
-const Menu = ({ history }) => (
-  
 
-  <div>
-    <ul className="nav nav-tabs bg-dark">
-    {isAuthenticated() && (
-        <li className="nav-item mr-auto">
-          <span
-            className="nav-link text-success">
-            Welcome {user.name}
-          </span>
+const Menu = ({ history }) => {
+  const {user} = isAuthenticated();
+    return(
+
+      <div>
+      <ul className="nav nav-tabs bg-dark">
+       {isAuthenticated() && (
+          <li className="nav-item mr-auto">
+            <span
+              className="nav-link text-success">
+              Welcome {user.name}
+            </span>
+          </li>
+        )}
+        <li className="nav-item">
+          <Link style={currentTab(history, "/")} className="nav-link" to="/">
+            Home
+          </Link>
         </li>
-      )}
-      <li className="nav-item">
-        <Link style={currentTab(history, "/")} className="nav-link" to="/">
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/cart")}
-          className="nav-link"
-          to="/cart"
-        >
-          Cart 
-        </Link>
-      </li>
-      {isAuthenticated() && isAuthenticated().user.role === 0 && (
-              <li className="nav-item">
-              <Link
-                style={currentTab(history, "/user/dashboard")}
-                className="nav-link"
-                to="/user/dashboard"
-              >
-                Dashboard
-              </Link>
-            </li>
-      )}
-     {isAuthenticated() && isAuthenticated().user.role === 1 && (
         <li className="nav-item">
-        <Link
-          style={currentTab(history, "/admin/dashboard")}
-          className="nav-link"
-          to="/admin/dashboard"
-        >
-          A. Dashboard
-        </Link>
-      </li>
-     )}
-
-      {!isAuthenticated() && (
-              <Fragment>
-              <li className="nav-item">
-                <Link
-                  style={currentTab(history, "/signup")}
-                  className="nav-link"
-                  to="/signup"
-                >
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  style={currentTab(history, "/signin")}
-                  className="nav-link"
-                  to="/signin"
-                  
-                >
-                  Sign In
-                </Link>
-              </li>
-              </Fragment>
-      )}
-      {isAuthenticated() && (
-        <li className="nav-item">
-          <span
-            className="nav-link text-danger"
-            onClick={() => {
-              signout(() => {
-                history.push("/");
-                
-              });
-              
-            }}
+          <Link
+            style={currentTab(history, "/cart")}
+            className="nav-link"
+            to="/cart"
           >
-            Signout
-          </span>
+            Cart 
+          </Link>
         </li>
-      )}
-     
+        {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                <Link
+                  style={currentTab(history, "/user/dashboard")}
+                  className="nav-link"
+                  to="/user/dashboard"
+                >
+                  Dashboard
+                </Link>
+              </li>
+        )}
+       {isAuthenticated() && isAuthenticated().user.role === 1 && (
+          <li className="nav-item">
+          <Link
+            style={currentTab(history, "/admin/dashboard")}
+            className="nav-link"
+            to="/admin/dashboard"
+          >
+            A. Dashboard
+          </Link>
+        </li>
+       )}
+  
+        {!isAuthenticated() && (
+                <Fragment>
+                <li className="nav-item">
+                  <Link
+                    style={currentTab(history, "/signup")}
+                    className="nav-link"
+                    to="/signup"
+                  >
+                    Signup
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    style={currentTab(history, "/signin")}
+                    className="nav-link"
+                    to="/signin"
+                    
+                  >
+                    Sign In
+                  </Link>
+                </li>
+                </Fragment>
+        )}
+        {isAuthenticated() && (
+          <li className="nav-item">
+            <span
+              className="nav-link text-danger"
+              onClick={() => {
+                signout(() => {
+                  history.push("/");
+                  
+                });
+                
+              }}
+            >
+              Signout
+            </span>
+          </li>
+        )}
+       
+  
+      </ul>
+    </div>
+    )
 
-    </ul>
-  </div>
-);
+
+};
 
 export default withRouter(Menu);
