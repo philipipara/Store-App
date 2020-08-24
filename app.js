5c2f2ba8179b6590314f63485a4792465c69c943
Philip <philip.iparaguirre@gmail.com>
2020-08-24 14:35:36 -0700

    updated

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ let stripeRoute = require("./routes/stripepayment");
   app.use("/api", productRoutes);
   app.use("/api", orderRoutes);
   app.use("/api", stripeRoute);
-  app.use('/api', expressJwt(process.env.SECRET));
+  
 
   //port
   const PORT = process.env.PORT || 8000;
